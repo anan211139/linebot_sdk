d2c2190f2aaa2cad066a5fc21f54699ba7da7843
Chayanin <chayanin2007@gmail.com>
2018-06-11 16:02:16 +0700

    edit pushID

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEP
 
 $channelSecret = '40f2053df45b479807d8f2bba1b0dbe2';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U9a9b0cd702b6581861d1c93957eeb9c4';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
